defe22943a35f177549b1819b51ad32e0fa7d723
lulvz <kekwm8@gmail.com>
2025-10-20 21:15:52 +0100

    Implement base LIDAR controller and wall follower class for robot navigation

--- assignment1/controller_robot1.py
+++ assignment1/controller_robot1.py
@@ -17,24 +17,48 @@ from rclpy.node import Node
 from geometry_msgs.msg import TwistStamped
 from sensor_msgs.msg import LaserScan
 
-class ControllerRobot1(Node):
+from assignment1.base_lidar_controller import BaseLidarController
 
-    def __init__(self):
+class WallFollowerLidarController(BaseLidarController):
+    def compute_velocity(self, scan_msg: LaserScan) -> tuple[float, float]:
+        linear_velocity = 0.2
+        angular_velocity = 0.5
+
+        return (linear_velocity, angular_velocity)
+
+class ControllerRobot1(Node):
+    def __init__(self, controllers: list[BaseLidarController]):
         super().__init__('controller_robot1')
+
+        self.controllers = controllers
+
         self.publisher_ = self.create_publisher(TwistStamped, '/robot1/cmd_vel', 10)
         self.subscribtion = self.create_subscription(LaserScan, '/robot1/scan', self.scan_callback, 10)
 
     def scan_callback(self, msg):
         cmd = TwistStamped()
-        
-        # Calculate desired speeds based on laserscan
-        min_range = min(msg.ranges)
-        if min_range < 0.5:
-            cmd.twist.angular.z = 0.5
+
+        linear_sum = 0.0
+        angular_sum = 0.0
+        num_controllers = len(self.controllers)
+
+        for controller in self.controllers:
+            linear, angular = controller.compute_velocity(msg)
+            linear_sum += linear
+            angular_sum += angular
+
+        if num_controllers > 0:
+            linear_avg = linear_sum / num_controllers
+            angular_avg = angular_sum / num_controllers
         else:
-            cmd.twist.linear.x = 0.15
+            linear_avg = 0.0
+            angular_avg = 0.0
         
-        # Publish desired speeds
+        cmd = TwistStamped()
+        cmd.header.stamp = self.get_clock().now().to_msg()
+        cmd.twist.linear.x = linear_avg
+        cmd.twist.angular.z = angular_avg
+        
         cmd.header.stamp = self.get_clock().now().to_msg()
         self.publisher_.publish(cmd)
         self.get_logger().info('Robot1 Publishing - v:"%f" w:"%f"' % (cmd.twist.linear.x, cmd.twist.angular.z))
@@ -43,13 +67,14 @@ class ControllerRobot1(Node):
 def main(args=None):
     rclpy.init(args=args)
 
-    controller_robot1 = ControllerRobot1()
+    wall_follower = WallFollowerLidarController()
+
+    controller_robot1 = ControllerRobot1(
+        controllers=[wall_follower]
+    )
 
     rclpy.spin(controller_robot1)
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     controller_robot1.destroy_node()
     rclpy.shutdown()
 
